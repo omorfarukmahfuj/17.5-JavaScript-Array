717b3a3d04a4129f03ca58fe69fb503d698d142a
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-01-28 23:14:22 +0600

    practice-task-02 added

---
+++ Practice Task/practice-task-02.js
@@ -0,0 +1,6 @@
+const touristDestinations = ['sundorban', 'sajek', 'coxsbazar'];
+touristDestinations.push('bandarban');
+touristDestinations.push('gazipur');
+touristDestinations.push('fantastic kingdom');
+touristDestinations.pop();
+console.log(touristDestinations);
