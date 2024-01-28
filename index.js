064d9faf951fb9957b39bd5d7de82f79182fc02f
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-01-28 19:22:23 +0600

    length & index added

---
+++ index.js
@@ -0,0 +1,11 @@
+const numbers = [88, 77, 45, 96, 140, 100, 52];
+console.log(numbers);
+
+// get elements value by index
+console.log(numbers[0]);
+const fourth = numbers[3];
+console.log(fourth);
+
+// set or update elements value by index
+numbers[1] = 55;
+console.log(numbers);
---
+++ length.js
@@ -0,0 +1,3 @@
+const numbers = [1, 2, 3, 44, 55, 6, 89, 0];
+
+console.log(numbers.length);
