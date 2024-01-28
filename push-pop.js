fdbcca2b6b0bfa503c5d299e994ac63ab0b45086
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-01-28 19:31:17 +0600

    push-pop added

---
+++ push-pop.js
@@ -0,0 +1,10 @@
+const numbers = [2, 22, 5, 55, 6, 66];
+
+console.log(numbers);
+numbers.push(7);
+numbers.push(77);
+numbers.push(8, 88, 100);
+console.log(numbers);
+numbers.pop();
+numbers.pop();
+console.log(numbers);
