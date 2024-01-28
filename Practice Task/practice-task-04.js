91aacbcedec1674f78369ed4b91cab64067ca662
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-01-28 23:22:36 +0600

    practice-task-04 added

---
+++ Practice Task/practice-task-04.js
@@ -0,0 +1,9 @@
+const name = 'Omor Faruk';
+const friends = ['Sayem', 'Nazmul', 'Mahir', 'Ratul', 'Sadei'];
+const age = 18;
+const empty = "[]";
+
+console.log(Array.isArray(name));
+console.log(Array.isArray(friends));
+console.log(Array.isArray(age));
+console.log(Array.isArray(empty));
